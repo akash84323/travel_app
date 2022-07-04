49a672412c5d4e623e82992e82a6f898e8f531db
akash84323 <chandra.akash.akash@gmail.com>
2022-07-04 11:02:50 +0530

    page layout done

---
+++ lib/pages/nav_pages/main_page.dart
@@ -0,0 +1,30 @@
+import 'package:flutter/material.dart';
+import 'package:travel_app/pages/nav_pages/bar_item_page.dart';
+import 'package:travel_app/pages/nav_pages/my_page.dart';
+import 'package:travel_app/pages/nav_pages/search_page.dart';
+
+class MainPage extends StatefulWidget {
+  const MainPage({super.key});
+
+  @override
+  State<MainPage> createState() => _MainPageState();
+}
+
+class _MainPageState extends State<MainPage> {
+  List pages = [BarItemPage(), SearchPage(), MyPage()];
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      bottomNavigationBar: BottomNavigationBar(
+        items: const [
+          BottomNavigationBarItem(label: 'Home', icon: Icon(Icons.apps)),
+          BottomNavigationBarItem(
+              label: 'Bar', icon: Icon(Icons.bar_chart_rounded)),
+          BottomNavigationBarItem(
+              label: 'Search', icon: Icon(Icons.search_rounded)),
+          BottomNavigationBarItem(label: 'My', icon: Icon(Icons.person)),
+        ],
+      ),
+    );
+  }
+}
---
+++ lib/pages/nav_pages/my_page.dart
@@ -0,0 +1,14 @@
+// ignore_for_file: avoid_unnecessary_containers
+
+import 'package:flutter/material.dart';
+
+class MyPage extends StatelessWidget {
+  const MyPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      child: const Center(child: Text('My Page')),
+    );
+  }
+}
